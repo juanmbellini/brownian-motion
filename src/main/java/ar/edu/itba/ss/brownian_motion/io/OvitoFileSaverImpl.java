087c02a82218fc1c2d85ff7abe67f384ddc9f605
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-06-14 05:22:03 -0300

    Improve the OvitoFileSaver
    - Now include both wall points
    - Also include colors (not implemented though, but methods exists)

--- src/main/java/ar/edu/itba/ss/brownian_motion/io/OvitoFileSaverImpl.java
+++ src/main/java/ar/edu/itba/ss/brownian_motion/io/OvitoFileSaverImpl.java
@@ -1,14 +1,12 @@
 package ar.edu.itba.ss.brownian_motion.io;
 
 import ar.edu.itba.ss.brownian_motion.models.BrownSystem;
+import ar.edu.itba.ss.brownian_motion.models.Particle;
+import ar.edu.itba.ss.brownian_motion.models.Wall;
 import ar.edu.itba.ss.g7.engine.io.OvitoFileSaver;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * The ovito file saver that will be used to output data that can be read from ovito to display an animation.
@@ -21,55 +19,132 @@ public class OvitoFileSaverImpl extends OvitoFileSaver<BrownSystem.BrownSystemSt
      *
      * @param filePath Path to the file to be saved.
      */
-    public OvitoFileSaverImpl(@Value("${custom.output.ovito}") final String filePath) {
+    public OvitoFileSaverImpl(final String filePath) {
         super(filePath);
     }
+
 
     @Override
     public void saveState(final Writer writer, final BrownSystem.BrownSystemState state, final int frame)
             throws IOException {
 
-        final Stream<StringBuilder> particles = state.getParticleStates().stream()
-                .map(particle -> new StringBuilder()
-                        .append(particle.getPosition().getX())
-                        .append(" ")
-                        .append(particle.getPosition().getY())
-                        .append(" ")
-                        .append(particle.getVelocity().getX())
-                        .append(" ")
-                        .append(particle.getVelocity().getY())
-                        .append(" ")
-                        .append(particle.getRadius())
-                );
-        final Stream<StringBuilder> walls = state.getWallStates().stream()
-                .map(wall -> new StringBuilder()
-                        .append(wall.getInitialPoint().getX())
-                        .append(" ")
-                        .append(wall.getInitialPoint().getY())
-                        .append(" ")
-                        .append(0.0)
-                        .append(" ")
-                        .append(0.0)
-                        .append(" ")
-                        .append(0.0001)
-                        .append(" ")
-                        .append(wall.getFinalPoint().getX())
-                        .append(" ")
-                        .append(wall.getFinalPoint().getY())
-                        .append(" ")
-                        .append(0.0)
-                        .append(" ")
-                        .append(0.0)
-                        .append(" ")
-                        .append(0.0001)
-                );
+        final StringBuilder data = new StringBuilder();
+        // First, headers
+        data.append(state.getParticleStates().size() + 2 * state.getWallStates().size())
+                .append("\n")
+                .append(frame)
+                .append("\n");
+        // Save particles
+        for (Particle.ParticleState particle : state.getParticleStates()) {
+            saveParticle(data, particle);
+        }
+        // Save walls
+        for (Wall.WallState wall : state.getWallStates()) {
+            saveWall(data, wall);
+        }
 
-        final List<String> finalList = Stream.concat(particles, walls)
-                .map(StringBuilder::toString)
-                .collect(Collectors.toList());
-        final String stringRepresentation = finalList.stream().collect(Collectors.joining("\n"));
-        writer.append(Integer.toString(finalList.size())).append("\n")
-                .append(Integer.toString(frame)).append("\n")
-                .append(stringRepresentation).append("\n");
+        // Append data into the Writer
+        writer.append(data);
+
+    }
+
+    /**
+     * Saves a {@link ar.edu.itba.ss.brownian_motion.models.Particle.ParticleState}
+     * into the {@code data} {@link StringBuilder}.
+     *
+     * @param data     The {@link StringBuilder} that is collecting data.
+     * @param particle The {@link ar.edu.itba.ss.brownian_motion.models.Particle.ParticleState} with the data.
+     */
+    private void saveParticle(final StringBuilder data, final Particle.ParticleState particle) {
+        data.append("")
+                .append(particle.getPosition().getX())
+                .append(" ")
+                .append(particle.getPosition().getY())
+                .append(" ")
+                .append(particle.getVelocity().getX())
+                .append(" ")
+                .append(particle.getVelocity().getY())
+                .append(" ")
+                .append(particle.getRadius())
+                .append(" ")
+                .append(calculateRedForParticle(particle)) // Red
+                .append(" ")
+                .append(calculateGreenForParticle(particle)) // Green
+                .append(" ")
+                .append(calculateBlueForParticle(particle)) // Blue
+                .append("\n");
+    }
+
+    /**
+     * Saves a {@link ar.edu.itba.ss.brownian_motion.models.Wall.WallState}
+     * into the {@code data} {@link StringBuilder}.
+     *
+     * @param data The {@link StringBuilder} that is collecting data.
+     * @param wall The {@link ar.edu.itba.ss.brownian_motion.models.Wall.WallState} with the data.
+     */
+    private void saveWall(final StringBuilder data, final Wall.WallState wall) {
+        data.append("")
+                .append(wall.getInitialPoint().getX())
+                .append(" ")
+                .append(wall.getInitialPoint().getY())
+                .append(" ")
+                .append(0)
+                .append(" ")
+                .append(0)
+                .append(" ")
+                .append(0.0001)  // Radius
+                .append(" ")
+                .append(1) // Red
+                .append(" ")
+                .append(1) // Green
+                .append(" ")
+                .append(1) // Blue
+                .append("\n")
+                .append(wall.getFinalPoint().getX())
+                .append(" ")
+                .append(wall.getFinalPoint().getY())
+                .append(" ")
+                .append(0)
+                .append(" ")
+                .append(0)
+                .append(" ")
+                .append(0.0001) // Radius
+                .append(" ")
+                .append(1) // Red
+                .append(" ")
+                .append(1) // Green
+                .append(" ")
+                .append(1) // Blue
+                .append("\n");
+    }
+
+    /**
+     * Provides the red component for the given particle.
+     *
+     * @param particleState The particle whose red color component is going to be calculated.
+     * @return The red component for the particle.
+     */
+    private double calculateRedForParticle(final Particle.ParticleState particleState) {
+        return 1d;
+    }
+
+    /**
+     * Provides the green component for the given particle.
+     *
+     * @param particleState The particle whose green color component is going to be calculated.
+     * @return The green component for the particle.
+     */
+    private double calculateGreenForParticle(final Particle.ParticleState particleState) {
+        return 1d;
+    }
+
+    /**
+     * Provides the blue component for the given particle.
+     *
+     * @param particleState The particle whose blue color component is going to be calculated.
+     * @return The blue component for the particle.
+     */
+    private double calculateBlueForParticle(final Particle.ParticleState particleState) {
+        return 1d;
     }
 }
